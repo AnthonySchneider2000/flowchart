c8ae36c5afdf4cd0b4dc8816fddec9689eff0fc2
Anthony Schneider <tonyschneider3@gmail.com>
2025-03-20 13:19:28 -0500

    init

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,3 @@
+Use bun as the package manager.
+Use Powershell syntax for commands- for example, use semicolons (;) to concatonate commands, NOT &&.
+Use shadcn components for styling. These can be added with `bunx --bun shadcn@latest add <component>`
---
+++ app/layout.tsx
@@ -0,0 +1,72 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Flowchart",
+  description: "A flowchart tool",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="relative flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex-1">{children}</main>
+            </div>
+          </SidebarProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-10 w-full border-b bg-background">
+      <div className="flex h-16 items-center w-full px-4">
+        <SidebarTrigger />
+
+        {/* <MenuButton /> */}
+        <div className="flex-1 flex justify-center font-medium">Title</div>
+        <div className="w-14 flex justify-end">
+          <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  );
+
+  function MenuButton() {
+    return <div className="w-14">
+      <Button variant="ghost" size="icon">
+        <Menu className="h-5 w-5" />
+        <span className="sr-only">Menu</span>
+      </Button>
+    </div>;
+  }
+}
